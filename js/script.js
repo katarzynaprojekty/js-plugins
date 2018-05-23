fc1c421a2962afda8d15634799af6bf689e40fe0
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-05-23 16:42:44 +0200

    Add Mustache.js to generate slides

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-/*
-var elem = document.querySelector('.main-carousel');
+var templateItem = document.getElementById('template-product-item').innerHTML;
 
-var flkty = new Flickity(elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-*/
+Mustache.parse(templateItem);
 
+var listItems = '';
+
+for(var i = 0; i < sliderData.length; i++){
+    listItems += Mustache.render(templateItem, sliderData[i]);
+}
+
+var results = document.getElementById('results');
+results.insertAdjacentHTML('beforeend', listItems);
+
+
+
+//Flickity zadanie 10.1
 var flkty = new Flickity('.main-carousel', {
   // options
     cellAlign: 'left',
@@ -17,52 +23,14 @@ var flkty = new Flickity('.main-carousel', {
     percentPosition: false,
 });
 
-//zad11
 var restartButton = document.querySelector('#restart');
 restartButton.addEventListener( 'click', function() {
   flkty.select(0);
 });
 
-//zad12
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
-
-/*
-//zad 12v2
-//keyhole parallax
-var imgs = carousel.querySelectorAll('.carousel-cell img');
-// get transform property
-var docStyle = document.documentElement.style;
-var transformProp = typeof docStyle.transform == 'string' ?
-  'transform' : 'WebkitTransform';
-
-flkty.on( 'scroll', function() {
-  flkty.slides.forEach( function( slide, i ) {
-    var img = imgs[i];
-    var x = ( slide.target + flkty.x ) * -1/3;
-    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
-  });
-});
-*/
-
-
-
-
-
-
-
-
-/*
-requirejs( [ 'vendor/flickity-hash' ], function( Flickity ) {
-    var flkty = new Flickity('.main-carousel', {
-      // options
-        cellAlign: 'left',
-        contain: true,
-        hash: true
-    });
-});
-*/
